dc58e5b01f5a82ab33342a7079b11dbdb16c64d1
Samuel Andrade de Araújo <andradedearaujo100@gmail.com>
2024-06-11 14:07:28 -0300

    Corrigindo criação dos modelos no banco de dados.

--- project/backend/src/index.js
+++ project/backend/src/index.js
@@ -1,20 +1,27 @@
-import { Sequelize } from 'sequelize';
+import { Sequelize } from 'sequelize'
 import Disciplina from './models/disciplina.js'
 import Professor from './models/professor.js'
 import Sala from './models/sala.js'
 import Turma from './models/turma.js'
-import dotenv from 'dotenv';
+import dotenv from 'dotenv'
 
-dotenv.config();
+dotenv.config()
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
-});
+})
 
 Disciplina.init(sequelize)
-Professor.init(sequelize)
-Sala.init(sequelize)
-Turma.init(sequelize)
+Disciplina.sync({ alter: true })
 
-export default sequelize;
+Professor.init(sequelize)
+Professor.sync({ alter: true })
+
+Sala.init(sequelize)
+Sala.sync({ alter: true })
+
+Turma.init(sequelize)
+Turma.sync({ alter: true })
+
+export default sequelize
